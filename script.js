a56fa3ea161cfe8f8210ef6673b22fa638228513
Terry LIn <terry1997116@gmail.com>
2019-08-10 14:54:52 +0800

    javascript tutorial

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 console.log('my first javascript code');
 
 var firstname = 'terry';
@@ -6,4 +7,47 @@ var lastname = 'lin';
 
 var age = 19;
 
-console.log(firstname + lastname + '\nhis name is' + ' ' + age);
+console.log(firstname + lastname + '\n his name is' + ' ' + age);
+
+alert(firstname + lastname + '\n his name is' + ' ' + age);
+
+var jobs = prompt('what is his job?');
+console.log(firstname + ' ' + lastname + ' ' + 'his job is ' + jobs);
+*/
+
+
+
+//operator//
+/*
+var year, month, date;
+year = 2019;
+month = year + 12;
+date = year + 8;
+console.log(date) 
+
+//logical
+
+var terryage = 19;
+var ninnieage = 11;
+
+var div = terryage - ninnieage;
+var comparsion = ninnieage > terryage
+
+console.log(div)
+console.log(comparsion)
+*/
+
+var username = 'terry';
+var age = 21;
+
+if (age < 13){
+    console.log(username + ' ' + 'is child');
+}else if (age > 13 && age < 20){
+    console.log(username + ' ' + 'is a teenager')
+}else{
+    console.log(username + ' ' + 'is adult')
+}
+
+
+
+
